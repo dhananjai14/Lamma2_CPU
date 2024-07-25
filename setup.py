b6a9727a17e09d960a275a80763de8fe1231b627
Dhananjai Singh <dhananjai.eee@gmail.com>
2024-07-25 18:32:40 +0530

    added requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'Dhananjai',
+    author_email= 'dhananjai.eee@gmail.com',
     packages= find_packages(),
     install_requires = []
 
